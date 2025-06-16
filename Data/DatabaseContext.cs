5b16f63f185e6c98ccc5879ff5ec17bdca1445d4
Leandro-Solany <leandrospmedeiros@gmail.com>
2025-06-15 21:25:13 -0300

    Finansmart

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -14,24 +14,24 @@ namespace Finansmart.Data
         public DbSet<Usuario> Usuarios { get; set; }
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
-        // Remova o construtor protegido se não estiver usando (boas práticas)
-        // protected DatabaseContext() { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Chave composta para CursoProgresso
             modelBuilder.Entity<CursoProgresso>()
                 .HasKey(cp => new { cp.CursoId, cp.UsuarioId });
 
+            // Precisão para AvaliacaoMedia do Curso
             modelBuilder.Entity<Curso>()
                 .Property(c => c.AvaliacaoMedia)
                 .HasPrecision(10, 2);
 
+            // Precisão para Valor em MovimentacaoFinanceira
             modelBuilder.Entity<MovimentacaoFinanceira>()
                 .Property(mf => mf.Valor)
                 .HasPrecision(12, 2);
 
-            // REMOVA HasConversion<int>() nesses casos:
-            // Apenas defina o tipo da coluna, se quiser garantir no banco:
+            // Campos booleanos modelados como int (0/1) - garante o tipo no banco
             modelBuilder.Entity<Desafio>()
                 .Property(d => d.Ativo)
                 .HasColumnType("NUMBER(1)");
@@ -46,6 +46,5 @@ namespace Finansmart.Data
 
             base.OnModelCreating(modelBuilder);
         }
-
     }
 }
